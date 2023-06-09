56a40f225c2cecae6d52818ecc63950df967287a
claudiocassimiro <days_surfing@live.com>
2023-06-08 21:06:05 -0300

    refactor: add hasPriority prop

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,10 @@ const Header = () => {
     <header className={styles.header}>
       <ImageComponent
         src="/images/logo.webp"
-        alt="logo da RD com texto Resultado digitais"
+        alt="logo da RD Station com texto Resultado digitais"
         width={isDesktop ? 147 : 100}
         height={isDesktop ? 36 : 24}
+        hasPriority
       />
 
       <p className={styles.headerText}>Gerador de Cartão de Visita</p>
