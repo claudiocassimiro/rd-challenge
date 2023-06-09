e91ce2db4e128d1b27a944b40f0930b1219454fc
claudiocassimiro <days_surfing@live.com>
2023-06-09 15:04:37 -0300

    feat: implements compositionPattern on Field component

---
+++ src/components/Form/Field.tsx
@@ -0,0 +1,7 @@
+import { HTMLAttributes } from "react";
+
+interface FieldProps extends HTMLAttributes<HTMLDivElement> {}
+
+export function Field(props: FieldProps) {
+  return <div {...props} />;
+}
