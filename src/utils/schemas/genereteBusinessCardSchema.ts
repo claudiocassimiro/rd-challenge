395aed8fecd487c89cfb8130c1d10921e6911ce2
claudiocassimiro <days_surfing@live.com>
2023-06-09 15:52:38 -0300

    fix: hotfix

--- src/utils/schemas/genereteBusinessCardSchema.ts
+++ src/utils/schemas/genereteBusinessCardSchema.ts
@@ -5,20 +5,16 @@ export const generateBusinessCardSchema = z.object({
   name: z
     .string()
     .nonempty("O nome é obrigatório")
-    .min(2, "O nome deve ter no mínimo 2 caracteres")
-    .transform((name) => {
-      name
-        .trim()
-        .split(" ")
-        .map((word) => {
-          return word[0].toLocaleUpperCase().concat(word.substring(1));
-        })
-        .join(" ");
-    }),
+    .min(2, "O nome deve ter no mínimo 2 caracteres"),
   phone: z
     .string()
     .nonempty("O número de telefone deve ter no mínimo 14 caracteres")
-    .refine(validator.isMobilePhone),
+    .refine(
+      (phone) =>
+        /^(\(\d{2}\)\s\d\s\d{4}-\d{4}|\(\d{2}\)\s\d{4}-\d{4})$/.test(phone) ||
+        /^\(\d{2}\)\s\d{4,5}-\d{4}$/.test(phone),
+      "Número inválido"
+    ),
   email: z
     .string()
     .nonempty("O e-mail é obrigatório")
