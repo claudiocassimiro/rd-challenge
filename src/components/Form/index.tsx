dbf7ab8d754c58b097dcf20fa30543b3e23855e2
claudiocassimiro <days_surfing@live.com>
2023-06-09 15:05:28 -0300

    feat: implements index to use with composition pattern with Form componet

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,90 +1,11 @@
-import { useWindowSize } from "@/utils/helpers";
-import ImageComponent from "../ImageComponent";
-import styles from "./styles.module.css";
-import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa";
+import { ErrorMessage } from "./ErrorMessage";
+import { Field } from "./Field";
+import { Input } from "./Input";
+import { Label } from "./Label";
 
-const Form = () => {
-  const { isDesktop } = useWindowSize();
-  return (
-    <div className={styles.container}>
-      <ImageComponent
-        src="/images/paint-man.webp"
-        alt="imagem de homem pintando uma tela"
-        width={isDesktop ? 471 : 269}
-        height={isDesktop ? 347 : 198}
-        className={styles.imageStyle}
-        hasPriority
-      />
-      <div className={styles.formContainer}>
-        <form className={styles.form}>
-          <div className={styles.containerInput}>
-            <label className={styles.label} htmlFor="">
-              Nome*
-            </label>
-            <input
-              type="text"
-              placeholder="digite seu nome"
-              className={styles.input}
-            />
-          </div>
-
-          <div className={styles.containerPhoneInputAndEmailInput}>
-            <div
-              className={`${styles.containerInput} ${styles.containerInputPhone}`}
-            >
-              <label className={styles.label} htmlFor="">
-                Telefone*
-              </label>
-              <input
-                type="text"
-                placeholder="(00) 0 0000-0000"
-                className={styles.input}
-              />
-            </div>
-
-            <div
-              className={`${styles.containerInput} ${styles.containerInputEmail}`}
-            >
-              <label className={styles.label} htmlFor="">
-                Email*
-              </label>
-              <input
-                type="email"
-                placeholder="nome@email.com"
-                className={styles.input}
-              />
-            </div>
-          </div>
-        </form>
-        <div className={styles.containerAlertTexts}>
-          <p className={styles.alertText}>
-            • Ao preencher o formulário, concordo * em receber comunicações de
-            acordo com meus interesses.
-          </p>
-          <p className={styles.alertText}>
-            • Ao informar meus dados, eu concordo com a{" "}
-            <Link
-              className={styles.linkStyle}
-              href="https://legal.rdstation.com/pt/privacy-policy/"
-              title="pólitica de privacidade"
-              target="_blank"
-            >
-              Política de privacidade.
-            </Link>
-          </p>
-          <p className={styles.changePermissionText}>
-            * Você pode alterar suas permissões de comunicação a qualquer tempo.
-          </p>
-        </div>
-
-        <button type="button" className={styles.button}>
-          <span className={styles.buttonText}>GERAR CARTÃO GRÁTIS</span>
-          <FaArrowRight size={19} />
-        </button>
-      </div>
-    </div>
-  );
+export const Form = {
+  Label,
+  Input,
+  ErrorMessage,
+  Field,
 };
-
-export default Form;
