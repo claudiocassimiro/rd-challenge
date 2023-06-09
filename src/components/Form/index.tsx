7e12762f305d329080064d8d35288b530e587030
claudiocassimiro <days_surfing@live.com>
2023-06-08 21:05:06 -0300

    refactor: implements lines to improve code

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -14,66 +14,75 @@ const Form = () => {
         width={isDesktop ? 471 : 269}
         height={isDesktop ? 347 : 198}
         className={styles.imageStyle}
+        hasPriority
       />
-      <form className={styles.form}>
-        <div className={styles.containerInput}>
-          <label className={styles.label} htmlFor="">
-            Nome*
-          </label>
-          <input
-            type="text"
-            placeholder="digite Seu Nome"
-            className={styles.input}
-          />
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <div className={styles.containerInput}>
+            <label className={styles.label} htmlFor="">
+              Nome*
+            </label>
+            <input
+              type="text"
+              placeholder="digite seu nome"
+              className={styles.input}
+            />
+          </div>
+
+          <div className={styles.containerPhoneInputAndEmailInput}>
+            <div
+              className={`${styles.containerInput} ${styles.containerInputPhone}`}
+            >
+              <label className={styles.label} htmlFor="">
+                Telefone*
+              </label>
+              <input
+                type="text"
+                placeholder="(00) 0 0000-0000"
+                className={styles.input}
+              />
+            </div>
+
+            <div
+              className={`${styles.containerInput} ${styles.containerInputEmail}`}
+            >
+              <label className={styles.label} htmlFor="">
+                Email*
+              </label>
+              <input
+                type="email"
+                placeholder="nome@email.com"
+                className={styles.input}
+              />
+            </div>
+          </div>
+        </form>
+        <div className={styles.containerAlertTexts}>
+          <p className={styles.alertText}>
+            • Ao preencher o formulário, concordo * em receber comunicações de
+            acordo com meus interesses.
+          </p>
+          <p className={styles.alertText}>
+            • Ao informar meus dados, eu concordo com a{" "}
+            <Link
+              className={styles.linkStyle}
+              href="https://legal.rdstation.com/pt/privacy-policy/"
+              title="pólitica de privacidade"
+              target="_blank"
+            >
+              Política de privacidade.
+            </Link>
+          </p>
+          <p className={styles.changePermissionText}>
+            * Você pode alterar suas permissões de comunicação a qualquer tempo.
+          </p>
         </div>
 
-        <div className={styles.containerInput}>
-          <label className={styles.label} htmlFor="">
-            Telefone*
-          </label>
-          <input
-            type="text"
-            placeholder="(00) 0 0000-0000"
-            className={styles.input}
-          />
-        </div>
-
-        <div className={styles.containerInput}>
-          <label className={styles.label} htmlFor="">
-            Email*
-          </label>
-          <input
-            type="email"
-            placeholder="nome@email.com"
-            className={styles.input}
-          />
-        </div>
-      </form>
-      <div className={styles.containerAlertTexts}>
-        <p className={styles.alertText}>
-          • Ao preencher o formulário, concordo * em receber comunicações de
-          acordo com meus interesses.
-        </p>
-        <p className={styles.alertText}>
-          • Ao informar meus dados, eu concordo com a{" "}
-          <Link
-            className={styles.linkStyle}
-            href="#"
-            title="pólitica de privacidade"
-            target="_blank"
-          >
-            Política de privacidade.
-          </Link>
-        </p>
-        <p className={styles.changePermissionText}>
-          * Você pode alterar suas permissões de comunicação a qualquer tempo.
-        </p>
+        <button type="button" className={styles.button}>
+          <span className={styles.buttonText}>GERAR CARTÃO GRÁTIS</span>
+          <FaArrowRight size={19} />
+        </button>
       </div>
-
-      <button type="button" className={styles.button}>
-        <span className={styles.buttonText}>GERAR CARTÃO GRÁTIS</span>
-        <FaArrowRight size={19} />
-      </button>
     </div>
   );
 };
