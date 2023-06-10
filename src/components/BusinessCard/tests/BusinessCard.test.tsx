71838e3e86487a0d2cac96c644f2743015f6b4de
claudiocassimiro <days_surfing@live.com>
2023-06-10 11:48:29 -0300

    refactor: implements imporves to test file

--- src/components/BusinessCard/tests/BusinessCard.test.tsx
+++ src/components/BusinessCard/tests/BusinessCard.test.tsx
@@ -9,19 +9,21 @@ describe("BusinessCard", () => {
 
   const customGlobal: any = global;
 
-  describe("When the component BusinessCard is rendered on", () => {
-    const setCardData = vi.fn();
-    const cardData = {
+  const mockProps = {
+    setCardData: vi.fn(),
+    cardData: {
       name: "Cláudio Cassimiro",
       phone: "(81) 9 9352-0671",
       email: "test@gmail.com",
-    };
+    },
+  };
 
+  describe("When the component BusinessCard is rendered on", () => {
     it("mobile devices, must not have the image with alt 'Imagem de homem pintando uma tela'", () => {
       customGlobal.innerWidth = 360;
       customGlobal.innerHeight = 640;
 
-      render(<BusinessCard cardData={cardData} setCardData={setCardData} />);
+      render(<BusinessCard {...mockProps} />);
 
       expect(
         screen.queryByAltText("Imagem de homem pintando uma tela")
@@ -32,7 +34,7 @@ describe("BusinessCard", () => {
       customGlobal.innerWidth = 1220;
       customGlobal.innerHeight = 720;
 
-      render(<BusinessCard cardData={cardData} setCardData={setCardData} />);
+      render(<BusinessCard {...mockProps} />);
 
       expect(
         screen.getByAltText("Imagem de homem pintando uma tela")
@@ -41,15 +43,8 @@ describe("BusinessCard", () => {
   });
 
   describe("When the component BusinessCard is rendered with values on prop cardData", () => {
-    const setCardData = vi.fn();
-    const cardData = {
-      name: "Cláudio Cassimiro",
-      phone: "(81) 9 9352-0671",
-      email: "test@gmail.com",
-    };
-
     it("the values on prop cardData, should be rendered on component", () => {
-      render(<BusinessCard cardData={cardData} setCardData={setCardData} />);
+      render(<BusinessCard {...mockProps} />);
 
       const name = screen.getByText("Cláudio Cassimiro");
       const phone = screen.getByText("(81) 9 9352-0671");
